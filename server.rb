8efb10d654796cfd1d6af22a85ad121ba83b5395
lizconlan <lizconlan@gmail.com>
2013-03-11 02:59:59 +0000

    fixing typo in main file

--- server.rb
+++ server.rb
@@ -10,21 +10,8 @@ enable :sessions
 before do
   env = ENV["RACK_ENV"] ? ENV["RACK_ENV"] : "development"
   dbconfig = YAML::load(File.open 'config/database.yml')[env]
-  @title = if ENV['title'] ? ENV['title'] ? YAML::load(File.open 'config/site.yml')[:title]
-  if env == "production"
-    db = URI.parse(ENV['DATABASE_URL'])
-    ActiveRecord::Base.establish_connection(
-      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :port     => db.port,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
-    )
-  else
-    ActiveRecord::Base.establish_connection(dbconfig)
-  end
+  @title = if ENV['title'] ? ENV['title'] : YAML::load(File.open 'config/site.yml')[:title]
+  ActiveRecord::Base.establish_connection(dbconfig)
   @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
 end
 
